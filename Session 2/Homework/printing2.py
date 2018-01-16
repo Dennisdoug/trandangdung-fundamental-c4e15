382811f36d13e660ba1b341b2249973f6a19cdbe
Dung Tran <ddt1328@outlook.com.vn>
2018-01-16 01:01:32 -0800

    initial commit

--- Session 2/Homework/printing2.py
+++ Session 2/Homework/printing2.py
@@ -1,4 +1,4 @@
 num = int(input("Enter the number of stars: "))
 for i in range(1, num + 1):
-   print("*", end='')
+   print("*", end='X')
 print()
---
+++ Session 2/Homework/printing4.py
@@ -0,0 +1,6 @@
+m = int(input("Enter the number of columns: "))
+n = int(input("Enter the number of rows: "))
+
+for i in range (1, n+1):
+    print("*", end = 'x')
+    print('*'*m, end = '\n')
